850d5ccff99842a721fae26952f6c663213cdc52
SateesH1122 <sateeshpadamu@gmail.com>
2025-03-17 16:32:26 +0530

    Added email sending Option for contact us

--- src/app/components/landing_page/contact/contact.component.ts
+++ src/app/components/landing_page/contact/contact.component.ts
@@ -1,13 +1,46 @@
-import { Component } from '@angular/core';
+// import { Component } from '@angular/core';
+// import { NavbarComponent } from "../navbar/navbar.component";
+// import { FooterComponent } from "../footer/footer.component";
+
+// @Component({
+//   selector: 'app-contact',
+//   imports: [NavbarComponent, FooterComponent],
+//   templateUrl: './contact.component.html',
+//   styleUrl: './contact.component.css'
+// })
+// export class ContactComponent {
+
+// }
+
+import { Component, OnInit } from '@angular/core';
 import { NavbarComponent } from "../navbar/navbar.component";
 import { FooterComponent } from "../footer/footer.component";
+import emailjs, { EmailJSResponseStatus } from 'emailjs-com';
+import { UserService } from '../../../services/user.service';
 
 @Component({
   selector: 'app-contact',
   imports: [NavbarComponent, FooterComponent],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+  styleUrls: ['./contact.component.css']
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
 
+  email: string = '';
+  username: string = '';
+  constructor(private userservice: UserService) { }
+  ngOnInit(): void {
+    this.email = this.userservice.getUser().email;
+    this.username = this.userservice.getUser().username;
+  }
+  public sendEmail(e: Event) {
+    e.preventDefault();
+
+    emailjs.sendForm('service_mknr2ay', 'template_5bw74wb', e.target as HTMLFormElement, '_KqmKltuKVn1sIqWX')
+      .then((result: EmailJSResponseStatus) => {
+        alert('Email sent successfully!');
+      }, (error) => {
+        alert('Failed to send email. Please try again later.');
+      });
+  }
 }
